a0be175996f0def1d0bc5a5acd9161318fe84d8f
Arthur <arthur.bajt@gmail.com>
2021-03-29 15:15:06 +0200

    Pilier bougent

--- Scene/Pilier/Piliers.gd
+++ Scene/Pilier/Piliers.gd
@@ -4,16 +4,16 @@ extends Spatial
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+const VITESSE = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	scale.y = Globals.rng.randf_range(1.0, 3.0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	translation.x -= VITESSE * delta
 
 
 func _on_Area_body_shape_entered(body_id, body, body_shape, area_shape):
